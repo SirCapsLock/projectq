37cb5240f1be015dda150ec3b0d2418cea5a8443
renanav <renan.avrahami@gmail.com>
2016-04-16 11:55:07 -0400

    Add LinkedIn Omniauth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
+var Linkedin = require('node-linkedin')('app-id', 'secret', 'callback');
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
-module.exports = router;
+// module.exports = router;
+
+var linkedin = Linkedin.init('my_access_token');
+
+// If we want to specify a timeout for the HTTP request
+// var linkedin = Linkedin.init('my_access_token', {
+//     timeout: 60000
+// });
+
+// Using a library like `expressjs` the module will
+// redirect for you simply by passing `res`.
+app.get('/oauth/linkedin', function(req, res) {
+// This will ask for permisssions etc and redirect to callback url.
+    Linkedin.auth.authorize(res, scope);
+});
